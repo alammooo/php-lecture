06f2f72c2c64d77fed09ba911b7ea0f47e8ac0fd
alammooo <abdullah.alamm@gmail.com>
2023-02-01 23:36:10 +0700

    add if else for title and header

--- Reisebüro/inc/header.inc.php
+++ Reisebüro/inc/header.inc.php
@@ -1,21 +1,30 @@
-<?php 
+<?php
 
-if(!isset($pageTitle)){
-    $pageTitle = "empty"
-}
+// if(!isset($pageTitle)){
+//     $pageTitle = "empty"
+// }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" type="text/css" href="./styles/simple.css">
-    <title><?php echo $pageTitle; ?></title>
+    <?php if (!empty($pageTitle)) : ?>
+        <title>
+            <?php echo $pageTitle; ?>
+        </title>
+    <?php else : ?>
+        <title>Das moderne Reisebüro</title>
+    <?php endif; ?>
+
 </head>
+
 <body>
     <header>
         <h1>Das moderne Reisebüro</h1>
@@ -28,4 +37,6 @@ if(!isset($pageTitle)){
     </header>
 
     <main>
-        <h2><?php echo $pageTitle; ?></h2>
+        <?php if (!empty($pageTitle)) : ?>
+            <h2><?php echo $pageTitle; ?></h2>
+        <?php endif; ?>
