37215cf09b5f33f3617b993069439d22268a5972
alammooo <abdullah.alamm@gmail.com>
2023-02-02 09:52:57 +0700

    change header

--- Reisebüro/inc/footer.inc.php
+++ Reisebüro/inc/footer.inc.php
@@ -1,7 +1,7 @@
 </main>
 
 <footer>
-    <img style="border-radius: 5px; max-width: 100%; width: 250px;" src="images/pexels-negative-space-34153.jpg">
+    <img style="border-radius: 5px; max-width: 100%; width: 250px;" src="images/5395379.jpg">
     <p>Ein erstes Projekt vom PHP Kurs</p>
 </footer>
 
--- Reisebüro/inc/header.inc.php
+++ Reisebüro/inc/header.inc.php
@@ -53,7 +53,8 @@
 
             <?php if (!isset($page)) $page = ""; ?>
             <a href="./" <?php if ($page === "index") : ?> class="active" <?php endif;  ?>>Startseite</a>
-            <a href="./" <?php if ($page === "index") : ?> class="active" <?php endif;  ?>>Helsinki</a>
+            <a href="./helsinki.php" <?php if ($page === "helsinki") : ?> class="active" <?php endif;  ?>>Helsinki</a>
+            <a href="./mallorca.php" <?php if ($page === "mallorca") : ?> class="active" <?php endif;  ?>>Mallorca</a>
 
         </nav>
     </header>
@@ -69,54 +70,3 @@
         // }
 
         ?>
-
-        <!DOCTYPE html>
-        <html lang="en">
-
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-            <link rel="stylesheet" type="text/css" href="./styles/simple.css">
-            <?php if (!empty($pageTitle)) : ?>
-                <title>
-                    <?php echo $pageTitle; ?>
-                </title>
-            <?php else : ?>
-                <title>Das moderne Reisebüro</title>
-            <?php endif; ?>
-
-            <style>
-                a.active {
-                    background-color: var(--accent);
-                    border: 1px solid var(--accent);
-                    color: white !important;
-                }
-
-                a.active:hover {
-                    color: white !important;
-                    background-color: black;
-                    border: 1px solid black;
-                }
-            </style>
-        </head>
-
-        <body>
-            <header>
-                <h1>Das moderne Reisebüro</h1>
-
-                <nav>
-
-                    <?php if (!isset($page)) $page = ""; ?>
-                    <a href="./" <?php if ($page === "index") : ?> class="active" <?php endif;  ?>>Startseite</a>
-                    <a href="./" <?php if ($page === "helsinki") : ?> class="active" <?php endif;  ?>>Helsinki</a>
-                    <a href="./" <?php if ($page === "mallorca") : ?> class="active" <?php endif;  ?>>Mallorca</a>
-
-                </nav>
-            </header>
-
-            <main>
-                <?php if (!empty($pageTitle)) : ?>
-                    <h2><?php echo $pageTitle; ?></h2>
-                <?php endif; ?>
